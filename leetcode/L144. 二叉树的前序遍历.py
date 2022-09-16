6bdc8e7fea0c5195018ff48a7caa1ec2f750269c
chenhuanxiang <chenhuanx@kaisagroup.com>
2022-09-16 17:01:28 +0800

    二叉树的前序、中序遍历

---
+++ leetcode/L144. 二叉树的前序遍历.py
@@ -0,0 +1,94 @@
+# Definition for a binary tree node.
+from collections import deque
+from typing import Optional, List
+
+
+# 莫里斯算法
+# https://baijiahao.baidu.com/s?id=1712736660249781959&wfr=spider&for=pc
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        def preorder(node: TreeNode):
+            if node:
+                left = node.left
+                right = node.right
+                print(node.val)
+                result.append(node.val)
+                preorder(left)
+                preorder(right)
+
+        result = []
+        preorder(root)
+        return result
+
+    def preorderTraversal2(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        stack = deque()
+        stack.append(root)
+        node = stack.pop()
+        while node:
+            result.append(node.val)
+            left = node.left
+            right = node.right
+            if right:
+                stack.append(right)
+            if left:
+                stack.append(left)
+            if not stack:
+                break
+            node = stack.pop()
+        return result
+
+    def morris(self, root: Optional[TreeNode]) -> List[int]:
+        result = list()
+        mostRight = None
+        if not root:
+            return result
+        cur = root
+        while cur:
+            mostRight = cur.left
+            if mostRight:
+                while mostRight.right and mostRight!=cur:
+                    mostRight = mostRight.right
+                if mostRight.right == None:
+                    mostRight.right = cur
+                    cur = cur.left
+                    continue
+                else:
+                    mostRight.right = None
+            cur = cur.right
+
+
+if __name__ == "__main__":
+    root = TreeNode()
+    root.val = 1
+    left1 = TreeNode()
+    left1.val = 2
+    right1 = TreeNode()
+    right1.val = 3
+    left11 = TreeNode()
+    left11.val = 4
+    right12 = TreeNode()
+    right12.val = 5
+    left21 = TreeNode()
+    left21.val = 6
+    right22 = TreeNode()
+    right22.val = 7
+    root.left = left1
+    root.right = right1
+    left1.left = left11
+    left1.right = right12
+    right1.left = left21
+    right1.right = right22
+    # Solution().preorderTraversal(root)
+    # result = Solution().preorderTraversal2(root)
+    result = Solution().morris(root)
+    print(result)
--- leetcode/L94. 二叉树的中序遍历.py
+++ leetcode/L94. 二叉树的中序遍历.py
@@ -1,4 +1,7 @@
 # Definition for a binary tree node.
+from collections import deque
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -9,6 +12,47 @@ class TreeNode:
 from typing import List, Optional
 
 
+# 前序遍历 [1,None,2,3]
+# 中序遍历 [1,3,2]
+# 后序遍历 [3,None,2,1]
+
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        pass
+        def midOrder(node: TreeNode):
+            if node == None:
+                return node
+            midOrder(node.left)
+            result.append(node.val)
+            midOrder(node.right)
+
+        result = list()
+        midOrder(root)
+        return result
+
+    def inorderTraversal1(self, root: Optional[TreeNode]) -> List[int]:
+        result = list()
+        stack = deque()
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            result.append(root.val)
+            root = root.right
+        return result
+
+
+if __name__ == "__main__":
+    # left21 = TreeNode(3)
+    # right1 = TreeNode(2, left21)
+    # root = TreeNode(1, None, right1)
+    right22 = TreeNode(7)
+    left21 = TreeNode(6)
+    right12 = TreeNode(5)
+    left11 = TreeNode(4)
+    right1 = TreeNode(3, left21, right22)
+    left1 = TreeNode(2, left11, right12)
+    root = TreeNode(1, left1, right1)
+    # result = Solution().inorderTraversal(root)
+    result = Solution().inorderTraversal1(root)
+    print(result)
